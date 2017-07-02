208a3bac19a5a5878a0d71437d9cfd14182a2c7e
Laurent <laurent.f.camus@gmail.com>
2017-07-02 21:48:54 +0200

    .

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' @param path path directory containing the data files to import
+#' @param template template name of data file to process
+#' @return a list compiling the data imported
+#' @export
+getData <- function(path, template="bdf.bsme2.req") {
+
+
+
+}
