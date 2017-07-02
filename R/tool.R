eabfed573047f4d0740266f69a0fc660d60b066b
Laurent <laurent.f.camus@gmail.com>
2017-07-02 21:11:52 +0200

    .

---
+++ R/tool.R
@@ -0,0 +1,25 @@
+#' @param s string to convert to HTML-entities
+#' @return a string whose special characters are converted to HTML entities
+#' @export
+stringToHtmlEntities <- function(s) {
+
+  f_he.src <- "data/html-entities.xlsx"
+  f_he <- "data/html-entities.RData"
+ #source: https://www.freeformatter.com/html-entities.html
+  if (!exists("html.entities")) {
+    if (file.exists(f_he))
+      load(f_he)
+    else {
+      html.entities <- readxl::read_xlsx(path=f_he.src, range="A1:D331")
+      save(html.entities,file=f_he)
+    }
+  }
+
+  he <- html.entities[!is.na(html.entities[,c("Entity Name")]) & !is.na(html.entities$Character),]
+  s <- gsub("&","&amp;",s)
+  for (i in 2:nrow(he))
+    s <- gsub(he[i,]$Character,he[i,c("Entity Name")],s)
+
+  return(s)
+
+}
