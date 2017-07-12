b32c2ee100df9af0fd6b9eb6814ab0e0f7377a66
Laurent <laurent.f.camus@gmail.com>
2017-07-12 23:36:43 +0200

    .

--- R/ccze_footer.R
+++ R/ccze_footer.R
@@ -1,21 +1,34 @@
 ## ---- footer
 require(htmltools)
+
 tags$footer(style="font-size: 90%",
-  hr(),
-  p(style="text-align: left; font-style: italic; line-height: 50%;",
-    "Basé sur la méthodologie \u25CF NACE révision 2"),
-  tags$ol(class="laparent",style="font-style: italic; padding-left: 3px;",
-          tags$li(HTML("En plus des salaires bruts des employés, les co&ucirc;ts totaux de la main-d'&oelig;uvre incluent les coûts indirects tels que les cotisations sociales à la charge des employeurs et les imp&ocirc;ts liés à l'emploi.")),
-          tags$li(HTML("Ensemble de l'économie excluant l'agriculture, la p&ecirc;che et le secteur public"))
-  ),
-  p(style="text-align: left;",
-    "Source : Eurostat",
-    span(style="float:right;",
-         paste0("Réalisé le ",format(Sys.Date(),"%e %B %Y")))),
-  p(),
-  p(class="title", style="text-align: left; color: MidnightBlue; font-size: 100%",
-    "Banque de France",
-    span(style="float:right; font-style: italic;",
-         HTML("Zone euro &bull; Principaux indicateurs économiques et financiers")))
+            hr(),
+            p(style="text-align: left; font-style: italic; line-height: 50%;",
+              ifelse(params$lang=="FR",
+                     "Basé sur la méthodologie NACE révision 2",
+                     "Based on methodology NACE revision 2")),
+            tags$ol(class="laparent",style="font-style: italic; padding-left: 3px;",
+                    tags$li(HTML(ifelse(params$lang=="FR",
+                                        "En plus des salaires bruts des employés, les co&ucirc;ts totaux de la main-d'&oelig;uvre incluent les coûts indirects tels que les cotisations sociales à la charge des employeurs et les imp&ocirc;ts liés à l'emploi.",
+                                        "In addition to employees' gross earnings, total labour costs include indirect costs such as employers' social contributions and taxes connected to the
+employment."))),
+                    tags$li(HTML(ifelse(params$lang=="FR",
+                                        "Ensemble de l'économie excluant l'agriculture, la p&ecirc;che et le secteur public",
+                                        "Whole economy excluding agriculture, fishing and government sectors.")))
+            ),
+            p(style="text-align: left;",
+              "Source Eurostat",
+              span(style="float:right;",
+                   paste0(ifelse(params$lang=="FR",
+                                 "Réalisé le ",
+                                 "Produced on "),
+                          format(Sys.Date(),"%e %B %Y")))),
+            p(),
+            p(class="title", style="text-align: left; color: MidnightBlue; font-size: 100%",
+              "Banque de France",
+              span(style="float:right; font-style: italic;",
+                   HTML(ifelse(params$lang=="FR",
+                               "Zone euro &bull; Principaux indicateurs économiques et financiers",
+                               "The Euro Area &bull; Main economic and financial indicators"))))
 )
 ## ---- end
